9c998d1aee3202b28bd012f6b6d292b8e39e96b7
Jeffrey Dutton <jdutton@MVSD.ORG>
2020-01-08 17:14:37 -0500

    initial

---
+++ ComandTest1/4/2020/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+//import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // CAN addresses for Talons and Victors
+  public static final int dtLeft = 0, dtRight = 1;
+
+  // controller and joystick axis/button assignments
+  // public static final int raiseLiftAxis = 3, lowerLiftAxis = 2, spinAxis = 5,
+  // joystickPort = 0, controllerPort = 1, openButton = 6, closeButton = 5;
+
+  // Talon objects
+  public static WPI_TalonSRX leftDrive, rightDrive;
+
+  // Joystick objects
+  public static Joystick stick, controller;
+  // DriveTrain object
+  public static DifferentialDrive rd;
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+}
